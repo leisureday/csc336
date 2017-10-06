77292b3baf1c1013331e825e11f7c4615e6baf1f
Tianshu <zhutians@teach.cs.toronto.edu>
2017-10-06 15:18:21 -0400

    finished q3

---
+++ a1/a1_q3_b.m
@@ -0,0 +1,9 @@
+x1 = 1;
+x2 = 10;
+dx = 10^(-10);
+y1 = log(x1);
+y2 = log(x2);
+y1_hat = log(x1 + dx);
+y2_hat = log(x2 + dx);
+condition1 = abs((y1_hat - y1)/(y1))/abs(dx/x1)
+condition2 = abs((y2_hat - y2)/(y2))/abs(dx/x2)
